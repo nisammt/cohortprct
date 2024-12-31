3b95bed7924b7b8310060d19ac2586d70252db5a
nisammt <nisammt@gmai.com>
2024-12-31 18:28:17 +0530

    routepath block

--- index.js
+++ index.js
@@ -12,9 +12,9 @@ app.use(express.json())
 app.use(cors({origin:["http://localhost:5173","https://mycarte45-nisamudheen-mts-projects.vercel.app/"],credentials:true,methods:["GET", "POST","PUT","DELETE"]}))
 app.use(cookieParser())
 
-app.get("/", (req, res, next) => {
-    res.json("hello world");
-});
+//app.get("/",(req,res,next)=>{
+    //res.json("hello world")
+//});
 app.use("/main",mainRoute)
 
 app.all("*",(req, res) =>{
